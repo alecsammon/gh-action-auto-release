910c1e44ef0994c952586fd9ddf1b7a35a0aa238
Alec Sammon <alec.sammon@paddle.com>
2023-01-11 08:31:49 +0000

    Initial test code

---
+++ index.js
@@ -0,0 +1,55 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    const token = core.getInput("github-token", { required: true });
+    const octokit = github.getOctokit(token);
+
+    const labelNames = await getPullRequestLabelNames(octokit);
+    core.debug(`PR label names: ${labelNames}`);
+
+    const labels = getInputLabels();
+    core.debug(`Input labels: ${labels}`);
+
+    core.setOutput("result", labels);
+
+    core.debug("End");
+} catch (error) {
+    core.setFailed(error.message);
+}
+
+async function getPullRequestLabelNames(octokit) {
+    const owner = github.context.repo.owner;
+    const repo = github.context.repo.repo;
+    const commit_sha = github.context.sha;
+    core.debug(
+        `PR context - Owner: ${owner} Repo: ${repo} Commit_SHA: ${commit_sha}`
+    );
+
+    const response =
+        await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
+            owner,
+            repo,
+            commit_sha,
+        });
+    core.debug(`Retrieved commit data: ${response.data}`);
+
+    const pr = response.data.length > 0 && response.data[0];
+    core.debug(`Retrieved PR: ${pr}`);
+
+    return pr ? pr.labels.map((label) => label.name || "") : [];
+}
+
+function getInputLabels() {
+    const raw = core.getInput("labels", { required: true });
+    core.debug(`Get input "labels": ${raw}`);
+
+    const json = JSON.parse(raw);
+    core.debug(`Parsed as JSON: ${json}`);
+
+    return Array.isArray(json) ? json : [];
+}
+
+run().catch((err) => {
+    core.setFailed(`Action failed with error: ${err.message}`);
+});
